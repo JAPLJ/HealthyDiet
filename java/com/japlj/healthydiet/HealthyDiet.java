a40595c99b95ceee213fa04caf3e5cf85424fa6d
japlj <ys.qicos@gmail.com>
2016-02-19 02:43:59 +0900

    Create the base of the mod

---
+++ java/com/japlj/healthydiet/HealthyDiet.java
@@ -0,0 +1,37 @@
+package com.japlj.healthydiet;
+
+import com.japlj.healthydiet.proxy.CommonProxy;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = HealthyDiet.MODID,
+	 version = HealthyDiet.VERSION)
+public class HealthyDiet {
+	@Mod.Instance("HealthyDiet")
+	public static final String MODID = "healthydiet";
+	public static final String VERSION = "0.0.1";
+
+	@SidedProxy(clientSide="com.japlj.healthydiet.proxy.ClientProxy", 
+				serverSide="com.japlj.healthydiet.proxy.ServerProxy")
+	public static CommonProxy proxy;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent e) {
+		proxy.preInit(e);
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent e) {
+		proxy.init(e);
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent e) {
+		proxy.postInit(e);
+	}
+}
